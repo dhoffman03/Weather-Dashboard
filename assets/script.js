fc95b7eccbca3f2ca1682c25a54d2049759a3cd5
Dymond <dymond.hoffman03@gmail.com>
2022-08-18 03:30:07 -0700

    js done, comments added

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,35 @@
+//WHEN I search for a city
+//THEN I am presented with current and future conditions for that city and that city is added to the search history
 function citySearch() {
+    //declare cityName
     var cityName = titleCase($("#search-input")[0].value.trim());
 
+    //request url
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=f2c05a37295f90b7db7d346676e806e9";
 
     fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
 
+                //set date
                 $("#city-name")[0].textContent = cityName + " (" + moment().format('MM/DD/YYYY') + ")";
 
-                $("#historic-cities").append('<li class="btn btn-block list-group-item list-group-item city-name">' + cityName);
+                //create search history list and append to html
+                $("#historic-cities").append('<li class="list-group-item text-center city-name">' + cityName);
 
+                //declare latitude and longitude
                 const lat = data.coord.lat;
                 const lon = data.coord.lon;
 
                 var latLon = lat.toString() + " " + lon.toString();
 
+                //set localStorage key 
                 localStorage.setItem(cityName, latLon);
 
+                //request url with lat and lon parameters
                 apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=f2c05a37295f90b7db7d346676e806e9";
 
+                //check if city exists
                 fetch(apiURL).then(function (newResponse) {
                     if (newResponse.ok) {
                         newResponse.json().then(function (newData) {
@@ -30,18 +40,22 @@ function citySearch() {
                     }
                 })
             })
+        //if (!newResponse.ok), then show alert
         } else {
             alert("Cannot find city!");
         }
     })
 }
 
+//get city from list
 function getcity(coordinates) {
     apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&exclude=minutely,hourly&units=imperial&appid=f2c05a37295f90b7db7d346676e806e9";
 
     fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+
+                //run forecast functions
                 getCurrentForecast(data);
                 getFiveDayForecast(data);
 
@@ -51,23 +65,39 @@ function getcity(coordinates) {
 
 }
 
+//WHEN I view current weather conditions for that city
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed
+//get current weather and add to html
 function getCurrentForecast(data) {
+    //make weather cards visable
     $("#weather-content").addClass("visible");
 
+    //get weather icon and change resolution "@2x.png"
     $("#weather-icon")[0].src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
+    //get current temp
     $("#temp")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " F";
+    //get current wind-speed
     $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
+    //get current humidity
     $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + " %";
 }
 
-
+//WHEN I view future weather conditions for that city
+//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//get five-day forecast and add to html
 function getFiveDayForecast(data) {
     console.log(data);
 
+    //get date
+    //use .html() to add to index.html file
     ($("#day-1")).html($("#day-1").textContent = moment().add(1, "days").format("MM/DD/YYYY"));
+    //get daily[] image
     ($("#img-1")).html($("#img-1").attr("src", "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png"));
+    //get daily[] temp
     ($("#temp-1")).html($("#temp-1").textContent = "Temperature: " + data.daily[1].temp.day + " F");
+    //get daily[] wind-speed
     ($("#wind-1")).html($("#wind-1").textContent = "Wind Speed: " + data.daily[1].wind_speed.toFixed(1) + " MPH");
+    //get daily[] humidity
     ($("#humidity-1")).html($("#humidity-1").textContent = "Humidity: " + data.daily[1].humidity + " %");
 
     ($("#day-2")).html($("#day-2").textContent = moment().add(2, "days").format("MM/DD/YYYY"));
@@ -96,7 +126,7 @@ function getFiveDayForecast(data) {
 
 }
 
-// This function applies title case to a city name if there is more than one word.
+//applies title case to a city name if there is more than one word
 function titleCase(city) {
     var updatedCity = city.toLowerCase().split(" ");
     var returnedCity = "";
@@ -107,6 +137,7 @@ function titleCase(city) {
     return returnedCity;
 }
 
+//When search is clicked, run citySearch()
 $("#search-btn").on("click", function (e) {
     e.preventDefault();
 
@@ -114,9 +145,13 @@ $("#search-btn").on("click", function (e) {
 
 })
 
+//WHEN I click on a city in the search history
+//THEN I am again presented with current and future conditions for that city
+//When history city is clicked, load city data
 $("#history").on("click", ".city-name", function () {
 
     var coordinates = (localStorage.getItem($(this)[0].textContent)).split(" ");
+    //convert string to number
     coordinates[0] = parseFloat(coordinates[0]);
     coordinates[1] = parseFloat(coordinates[1]);
 
